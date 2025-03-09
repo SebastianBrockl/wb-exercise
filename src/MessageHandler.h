2a2c84a33c1f48e7d6bb371ef9f17f62d46ec049
Sebastian Bröckl <sebastian.brockl@abo.fi>
2025-03-10 00:03:56 +0200

    started implementing MessageHandler class

--- src/MessageHandler.h
+++ src/MessageHandler.h
@@ -2,6 +2,8 @@
 #define MESSAGE_HANDLER_H
 
 #include "TLV.h"
+#include <vector>
+#include <memory>
 
 namespace msg
 {
@@ -9,10 +11,17 @@ namespace msg
     class MessageHandler
     {
     public:
-        MessageHandler() = default;
+        MessageHandler(std::unique_ptr<std::vector<uint8_t>> message_data);
         ~MessageHandler() = default;
 
         void handle_message(const std::vector<uint8_t> &message);
+        // deserialize message bytes to vector of tlv structs
+        std::vector<tlv> deserialize_message_data(const std::vector<uint8_t> &message_data);
+
+    private:
+        std::unique_ptr<std::vector<uint8_t>> m_message_bytes;
+        std::vector<tlv> m_message_contents;
+        FrameHeader m_frame_header;
     };
 
 }
---
+++ src/message_handler.cpp
@@ -0,0 +1,68 @@
+#include "MessageHandler.h"
+#include "util.h"
+#include "TLV.h"
+#include <iostream>
+
+using namespace msg;
+
+/**
+ * @brief Construct a new Message Handler object
+ *
+ * @param message_data unique pointer to a vector of uint8_t containing the message data bytes
+ *                     The message data is expected to be a complete message, including the header
+ *                     the ownership of the message data is transferred to the MessageHandler
+ */
+MessageHandler::MessageHandler(std::unique_ptr<std::vector<uint8_t>> message_data)
+    : m_message_bytes(std::move(message_data))
+{
+}
+
+std::vector<tlv> MessageHandler::deserialize_message_data(const std::vector<uint8_t> &message_data)
+{
+    if (message_data.size() < sizeof(FrameHeader))
+    {
+        auto message = "MessageHandler: Message data too short for header deserialization! "
+        + "Expected at least " + std::to_string(sizeof(FrameHeader)) + " bytes, got " + std::to_string(message_data.size());
+        throw std::runtime_error(message);
+    }
+    std::vector<tlv> tlvs;
+    // grab first 40 bytes to form header
+    auto iterator = message_data.begin();
+    auto header = util::deserialize_header(message_data, iterator);
+}
+
+void MessageHandler::handle_message(const std::vector<uint8_t> &message)
+{
+    // auto tlvs = deserialize_message_data(message);
+    // for (const auto &tlv : tlvs)
+    // {
+    //     std::visit([](auto &&arg) {
+    //         using T = std::decay_t<decltype(arg)>;
+    //         if constexpr (std::is_same_v<T, CompressedPointCloudTLV>)
+    //         {
+    //             std::cout << "Compressed Point Cloud TLV" << std::endl;
+    //         }
+    //         else if constexpr (std::is_same_v<T, TargetListTLV>)
+    //         {
+    //             std::cout << "Target List TLV" << std::endl;
+    //         }
+    //         else if constexpr (std::is_same_v<T, targetIndexTLV>)
+    //         {
+    //             std::cout << "Target Index TLV" << std::endl;
+    //         }
+    //         else if constexpr (std::is_same_v<T, PresenceIndicationTLV>)
+    //         {
+    //             std::cout << "Presence Indication TLV" << std::endl;
+    //         }
+    //         else if constexpr (std::is_same_v<T, TargetHeightTLV>)
+    //         {
+    //             std::cout << "Target Height TLV" << std::endl;
+    //         }
+    //         else
+    //         {
+    //             static_assert(always_false<T>::value, "non-exhaustive visitor!");
+    //         }
+    //     },
+    //                tlv);
+    // }
+}
