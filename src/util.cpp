4a3d78d9ce8637e690a6c9fe90fd72e4f5352b3b
Sebastian Bröckl <sebastian.brockl@abo.fi>
2025-03-10 03:13:03 +0200

    added initial target list deserialization functionality

--- src/util.cpp
+++ src/util.cpp
@@ -175,7 +175,7 @@ FrameHeader util::deserialize_header(const std::vector<uint8_t> &data)
  * @return FrameHeader
  * @throw std::runtime_error if data size is smaller than header size
  */
-FrameHeader util::deserialize_header(const std::vector<uint8_t> &data,  std::vector<uint8_t>::const_iterator iterator)
+FrameHeader util::deserialize_header(const std::vector<uint8_t> &data, std::vector<uint8_t>::const_iterator iterator)
 {
     FrameHeader header;
     if (data.size() < sizeof(FrameHeader))
@@ -184,7 +184,8 @@ FrameHeader util::deserialize_header(const std::vector<uint8_t> &data,  std::vec
     }
 
     // lambda function to copy data from iterator to field
-    auto copy_from_iterator = [&iterator](auto &field) {
+    auto copy_from_iterator = [&iterator](auto &field)
+    {
         std::memcpy(&field, &(*iterator), sizeof(field));
         std::advance(iterator, sizeof(field));
     };
@@ -210,4 +211,160 @@ FrameHeader util::deserialize_header(const std::vector<uint8_t> &data,  std::vec
     header.subFrameNumber = boost::endian::little_to_native(header.subFrameNumber);
 
     return header;
+}
+
+/**
+ * @brief Deserializes next TLV (type-length-value) objects from the incoming data stream.
+ *
+ * This function deserializes the next TLV object from the incoming data stream. The function assumes that the byte order is little endian.
+ *
+ * @param data The incoming data stream
+ * @param iterator The iterator to the current position in the data stream
+ * @return std::optional<tlv> The deserialized TLV object§
+ */
+std::optional<tlv> util::deserialize_next_tlv(const std::vector<uint8_t> &data, std::vector<uint8_t>::const_iterator &iterator)
+{
+    // lambda function to copy data from iterator to field
+    auto copy_from_iterator = [&iterator](auto &field)
+    {
+        std::memcpy(&field, &(*iterator), sizeof(field));
+        std::advance(iterator, sizeof(field));
+    };
+
+    uint32_t type;
+    uint32_t length;
+    copy_from_iterator(type);
+    copy_from_iterator(length);
+
+    type = boost::endian::little_to_native(type);
+    length = boost::endian::little_to_native(length);
+
+    switch (type)
+    {
+    // case 1020: // pointCloud
+    // {
+
+    // }
+    case 1010: // targetObjectList
+    {
+        TlvHeader header{TLVType::TargetObjectList, length};
+        return util::deserialize_target_list_tlv(data, iterator, header);
+        // return std::nullopt; // FIXME: delete this line
+    }
+    // case 1011: // targetIndex
+    // {
+
+    // }
+    // case 1012: // targetHeight
+    // {
+
+    // }
+    // case 1021: // presenceIndication
+    // {
+
+    // }
+    default:
+    {
+        std::cerr << "Util: deserialize_next_tlv: Unknown TLV type: " << type << std::endl;
+        std::advance(iterator, length); // advance iterator to skip entire TLV
+        return std::nullopt;
+    }
+    }
+}
+
+std::optional<TargetListTLV> deserialize_target_list_tlv(
+    const std::vector<uint8_t> &data,
+    std::vector<uint8_t>::const_iterator &iterator,
+    TlvHeader header)
+{
+    // lambda function to copy data from iterator to field
+    // auto copy_from_iterator = [&iterator](auto &field)
+    // {
+    //     std::memcpy(&field, &(*iterator), sizeof(field));
+    //     std::advance(iterator, sizeof(field));
+    // };
+
+    // check that there is sufficient data to contain the (possibly multiple) TLV's
+    const auto bytes_in_tlv = static_cast<size_t>(header.length) - sizeof(TlvHeader);
+    // naively assume that this cast is safe, should be since the distance should not be negative
+    const auto bytes_in_buffer = static_cast<size_t>(std::distance(iterator, data.end()));
+    if (bytes_in_buffer < bytes_in_tlv)
+    {
+        // we don't have enough data to contain even one target list TLV
+        std::cerr << "Util: deserialize_target_list_tlv: insufficient data: data size (" << bytes_in_buffer
+                  << ") is smaller than TLV size (" << bytes_in_tlv << ")" << std::endl;
+        return std::nullopt;
+    }
+    // we have data for at least one TLV, but there might be multiple
+    // in case we have multiple TLV's, ensure we have space and that the data is aligned
+    auto byte_misalignment = bytes_in_tlv % sizeof(Target);
+
+    // assemble TargetListTLV
+    TargetListTLV target_list;
+    target_list.header = header;
+
+    for (size_t i = 0; i < bytes_in_tlv / sizeof(Target); ++i)
+    {
+        Target target = util::deserialize_next_target(data, iterator);
+        target_list.targets.push_back(target);
+    }
+    if (byte_misalignment != 0)
+    {
+        std::cerr << "Util: deserialize_target_list_tlv: data is not aligned to target size " << byte_misalignment << "bytes misaligned" << std::endl;
+        std::advance(iterator, byte_misalignment);
+    }
+
+    return {target_list};
+}
+
+Target util::deserialize_next_target(const std::vector<uint8_t> &data, std::vector<uint8_t>::const_iterator &iterator)
+{
+    Target target;
+    // lambda function to copy data from iterator to field
+    auto copy_from_iterator = [&iterator](auto &field)
+    {
+        std::memcpy(&field, &(*iterator), sizeof(field));
+        field = boost::endian::little_to_native(field);
+        std::advance(iterator, sizeof(field));
+    };
+
+    // lambda function to copy data from iterator to field of type float
+    // this is required as boost::endian::little_to_native doesn't support floats
+    // blindly trusting that the target architecture has 32b floats
+    auto copy_float_from_iterator = [&iterator](float &field)
+    {
+        // first deserialize bytes to uint32_t
+        uint32_t pseudo_float;
+        std::memcpy(&pseudo_float, &(*iterator), sizeof(pseudo_float));
+        pseudo_float = boost::endian::little_to_native(pseudo_float);
+
+        // convert deserialized uint32_t to float
+        std::memcpy(&field, &pseudo_float, sizeof(pseudo_float));
+        std::advance(iterator, sizeof(pseudo_float));
+    };
+
+    // boost::endian::little_to_native doesn't support floats, so w need to work around
+    uint32_t my_ec[16];
+
+    copy_from_iterator(target.trackId);
+    copy_float_from_iterator(target.posx);
+    copy_float_from_iterator(target.posy);
+    copy_float_from_iterator(target.posz);
+    copy_float_from_iterator(target.velx);
+    copy_float_from_iterator(target.vely);
+    copy_float_from_iterator(target.velz);
+    copy_float_from_iterator(target.accx);
+    copy_float_from_iterator(target.accy);
+    copy_float_from_iterator(target.accz);
+    for (int i = 0; i < 16; i++)
+    {
+        // copy_from_iterator(my_ec[i]);
+        // // convert deserialized uint32_t to float
+        // std::memcpy(&target.ec[i], &my_ec[i], sizeof(my_ec[i]));
+        copy_float_from_iterator(target.ec[i]);
+    }
+    copy_float_from_iterator(target.g);
+    copy_float_from_iterator(target.confidencelevel);
+
+    return target;
 }
--- src/util.h
+++ src/util.h
@@ -4,6 +4,7 @@
 #include "TLV.h"
 #include <string>
 #include <memory>
+#include <optional>
 
 namespace util
 {
@@ -32,8 +33,14 @@ namespace util
      */
     void remove_comments(std::string &str);
 
+    // TLV handling & serialization utilities
+
+    // Serialize a TLV to a string
     std::string to_string(const FrameHeader& header);
+    // Serialize a TLV to a string with hex formatting
     std::string to_hex_string(const FrameHeader& header);
+
+    // helpers for serializing data fields to hex strings
 
     std::string to_hex_string(const std::vector<uint8_t> &data);
     std::string to_hex_string(const uint8_t &data);
@@ -41,8 +48,22 @@ namespace util
     std::string to_hex_string(const uint32_t &data);
     std::string to_hex_string(const uint64_t &data);
 
+    // deserialize header from byte stream
     FrameHeader deserialize_header(const std::vector<uint8_t> &data);
+    // deserialize header from byte stream, starting at iterator, iterator is incremented to point to the next byte after the header
     FrameHeader deserialize_header(const std::vector<uint8_t> &data,  std::vector<uint8_t>::const_iterator iterator);
+
+    // deserialize next TLV from byte stream, starting at iterator, iterator is incremented to point to the next byte after the TLV
+    std::optional<tlv> deserialize_next_tlv(const std::vector<uint8_t> &data, std::vector<uint8_t>::const_iterator &iterator);
+
+    // deserialize TargetListTLV from byte stream, starting at iterator, iterator is incremented to point to the next byte after the TLV
+    std::optional<TargetListTLV> deserialize_target_list_tlv(
+        const std::vector<uint8_t> &data, 
+        std::vector<uint8_t>::const_iterator &iterator,
+        TlvHeader header);
+
+    // deserialize targetIndexTLV Target from byte stream, starting at iterator, iterator is incremented to point to the next byte after the TLV
+    Target deserialize_next_target(const std::vector<uint8_t> &data, std::vector<uint8_t>::const_iterator &iterator);
 
 } // namespace util
 
